a5b22c29fb3597d22b3fe4c8d498800edc275960
Dewantara765 <rumiachan35@gmail.com>
2025-09-13 21:03:39 +0700

    hero responsif

--- src/App.vue
+++ src/App.vue
@@ -45,11 +45,11 @@ const openMenu = ref(false)
           <div>Komunitas</div>
           <div>ColorOS</div>
           <div>Dukungan</div>
-          <div>
+          <div class="flex">
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg>
             
-          </div>
-          <div>
+          
+        
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z"/></svg>
           </div>
 
@@ -75,13 +75,56 @@ const openMenu = ref(false)
       <MenuItem title="Oppo Pad Air" image="/dropdown/440-440-oppo-pad-air.webp"/>
       
   </div>
-  <section class="flex flex-col justify-start">
+  <section class="relative flex flex-col justify-start">
     <picture>
       <source srcset="/hero/id-s3-blue-s2-white-2-1536-800-pad-v3.jpg" alt="hero-1" type="image/webp" media="(min-width: 640px)">
       <source srcset="/hero/id-s3-blue-s2-white-2-720-1060-mo-v3.jpg" alt="hero-1" media="(max-width: 640px)">
       <img src="/hero/id-s3-blue-s2-white-2-720-1060-mo-v3.jpg" alt="hero-1">
     </picture>
 
+   
+      <img class="absolute inset-x-10 lg:inset-x-20 inset-y-8 z-10 w-30" src="/hero/homepage-black-pc.png"/>
+      <p class="absolute inset-x-10 lg:inset-x-20 inset-y-15 text-3xl lg:text-5xl">Reno14 Series <sup>5G</sup></p>
+      <p class="absolute inset-x-10 lg:inset-x-20 inset-y-30 text-xl lg:text-2xl">AI Flash Photography | AI Editor 2.0</p>
+      <p class="absolute inset-x-10 lg:inset-x-20 inset-y-45 lg:inset-y-40 text-xl lg:text-2xl">Underwater 4K Video</p>
+      <div class="absolute inset-x-10 lg:inset-x-20 inset-y-55 lg:inset-y-150 w-25 border-2 h-8 rounded-xl text-center">Lebih lanjut</div>
+      <div class="absolute inset-x-40 lg:inset-x-50 inset-y-55 lg:inset-y-150 w-30 border-2 h-8 rounded-xl text-center bg-black text-white">Beli sekarang</div>
+    
+  </section>
+
+  <section class="relative flex flex-col">
+    <picture>
+      <source srcset="/hero/1536-800.jpg" alt="hero-1" type="image/webp" media="(min-width: 640px)">
+      <source srcset="/hero/720-1060.jpg.thumb.webp" alt="hero-1" media="(max-width: 640px)">
+      <img src="/hero/720-1060.jpg.thumb.webp" alt="hero-1">
+    </picture>
+
+   
+      <img class="absolute inset-x-10 lg:inset-x-20 inset-y-8 z-10 w-30" src="/hero/homepage-black-pc.png"/>
+      <p class="absolute inset-x-10 lg:inset-x-20 inset-y-15 text-3xl lg:text-5xl">OPPO Find N5</p>
+      <img class="absolute inset-x-10 lg:inset-x-20 inset-y-30 w-50" src="/hero/hasu.png.thumb.webp">
+      <p class="absolute inset-x-10 lg:inset-x-20 inset-y-45 lg:inset-y-130 text-xl lg:text-2xl text-slate-700">Slim, Yet Powerful</p>
+      <div class="absolute inset-x-10 lg:inset-x-20 inset-y-55 lg:inset-y-150 w-25 border-2 h-8 rounded-xl text-center">Lebih lanjut</div>
+      <div class="absolute inset-x-40 lg:inset-x-50 inset-y-55 lg:inset-y-150 w-30 border-2 h-9 rounded-xl text-center bg-black text-white">Beli sekarang</div>
+    
+  </section>
+
+  <section class="relative flex flex-col">
+    <picture>
+      <source srcset="/hero/Pad.jpg" alt="hero-1" type="image/webp" media="(min-width: 640px)">
+      <source srcset="/hero/Mobile.jpg.thumb.webp" alt="hero-1" media="(max-width: 640px)">
+      <img src="/hero/Mobile.jpg.thumb.webp" alt="hero-1">
+    </picture>
+
+   
+      <img class="absolute inset-x-10 lg:inset-x-20 inset-y-8 z-10 w-30" src="/hero/homepage-black-pc.png"/>
+      <p class="absolute inset-x-10 lg:inset-x-20 inset-y-15 text-3xl lg:text-5xl">OPPO A5 Pro</p>
+      <p class="absolute inset-x-10 lg:inset-x-20 inset-y-25 text-lg lg:text-2xl">IP69, IP68, IP66 Tahan Air dan Debu</p>
+      <p class="absolute inset-x-10 lg:inset-x-20 inset-y-35 lg:inset-y-40 text-lg lg:text-2xl">Tahan Benturan Kelas Militer</p>
+      <p class="absolute inset-x-10 lg:inset-x-20 inset-y-45 lg:inset-y-50 text-lg lg:text-2xl">Baterai Besar 5800 mAh</p>
+      <div class="absolute inset-x-10 lg:inset-x-20 inset-y-55 lg:inset-y-150 w-25 border-2 h-8 rounded-xl text-center">Lebih lanjut</div>
+      <div class="absolute inset-x-40 lg:inset-x-50 inset-y-55 lg:inset-y-150 w-30 border-2 h-10 rounded-xl text-center bg-black text-white">Beli sekarang</div>
+    
   </section>
 
 
